c26bc2437146f837e992e584426ad77fcc44b0ff
Darryl Becker <dar.becker12@pm.me>
2022-12-10 16:09:01 -0700

    added all

---
+++ app.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { storeToRefs, mapActions} from "pinia";
+import {useCounterStore} from "./store/useCounter"
+
+const main = useCounterStore();
+
+const {counter, name } = storeToRefs(main);
+
+const { addOne } = main;
+
+function add(value: number) {
+  main.$patch(state => { state.counter += value
+  });
+}
+
+function reset() {
+  main.$reset();
+}
+</script>
+<template>
+  <div>
+    <h3>Hello</h3>
+    <h4>{{ counter}}</h4>
+    <button @click="add(15)">Click me</button>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir,Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
